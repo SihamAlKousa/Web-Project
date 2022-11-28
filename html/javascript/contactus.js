3c00db9e4e376f3aa6da9a788d3c76d75b76a0e8
AlaaLaghoub <U20104610@sharjah.ac.ae>
2022-11-28 08:18:55 +0400

    changes

--- html/javascript/contactus.js
+++ html/javascript/contactus.js
@@ -1,6 +1,27 @@
 /*project stage number 4
 Alaa laghoub
 U20104610*/
+
+///////////////elert window when user resets or submits///////////
+function showAlertS() {
+    if (confirm("Are you sure you want to submit your message? ")) {
+
+    } else {
+
+    }
+
+}
+function showAlertR() {
+
+    if (confirm("Are you sure you want to reset your message? ")) {
+
+    } else {
+
+    }
+
+}
+
+///////////////////////////STILL ADDING PHP FOR THIS TO WORK///////////////////////////////////
 const form = document.querySelector("form"),
 statusTxt = form.querySelector(".button-area span");
 
