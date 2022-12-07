8f13ea0d8986f7bdd68ab920f94b3e0ad7e276c1
AlaaLaghoub <U20104610@sharjah.ac.ae>
2022-12-08 00:35:52 +0400

    php for contactus

--- html/javascript/contactus.js
+++ html/javascript/contactus.js
@@ -21,26 +21,4 @@ function showAlertR() {
 
 }
 
-///////////////////////////STILL ADDING PHP FOR THIS TO WORK///////////////////////////////////
-const form = document.querySelector("form"),
-statusTxt = form.querySelector(".button-area span");
-
-form.onsubmit = (e) => {
-    e.preventDefualt(); // prevent form from submitting 
-    statusTxt.style.display = "block";
-
-    let xhr = new XMLHttpRequest();//creating new xml object
-    xhr.open("POST", "contactus.php", true); //sending post request to message.php file
-
-    xhr.onload = () => {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            let response = xhr.response; // storing ajax response in a response var
-            if(response.indexOf("Email and password field are required!")!= -1 || response.indexOf("Enter a valid email address!") || response.indexOf("Sorry, failed to send your message!")) {
-                statusTxt.style.color="red";
-            }
-            statusTxt.innerText = response;
-        }
-    }
-    let formData = new FormData(form);
-    xhr.send(formData);
 }
